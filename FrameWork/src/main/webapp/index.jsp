31fc430dbf4e4e04e71a72bb26ba28a25e146764
hyunwoo52014 <huywnoo50214@gmail.com>
2019-08-01 11:05:56 +0900

    jsp 수정

--- FrameWork/src/main/webapp/index.jsp
+++ FrameWork/src/main/webapp/index.jsp
@@ -1,5 +1,23 @@
+<%@page import="main.HelloWorld"%>
+<%@page import="main.Calcurator"%>
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<%!
+	HelloWorld he = new HelloWorld();
+	Calcurator cal = new Calcurator();
+%>
+<!DOCTYPE html>
 <html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello World!</h2><br>
+<h3><% System.out.print(he.getHello()); %></h3>
+덧셈 10+10 = <%cal.add(10, 10); %> <br>
+뺄셈 20-7 = <%cal.sub(20, 7); %> <br>
+나눗셈 10/10 = <%cal.div(10, 10); %> <br>
+곱셈 10*10 = <%cal.mul(10, 10); %> <br>
 </body>
 </html>
--- FrameWork/src/test/java/main/CalcuratorTest.java
+++ FrameWork/src/test/java/main/CalcuratorTest.java
@@ -1,31 +1,42 @@
 package main;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalcuratorTest {
 	
+	
+	private static Calcurator cal = null;
+	
+	@BeforeClass
+	public static void setCalcurator() throws Exception {
+		cal = new Calcurator();
+	}
+	
 	@Test
 	public void testSum() {
-		Calcurator c = new Calcurator();
-		assertEquals(60,c.add(30, 30),0);
+		assertEquals(60,cal.add(30, 30),0);
 	}
 	
 	@Test
 	public void testSub() {
-		Calcurator c = new Calcurator();
-		assertEquals(60,c.sub(90, 30),0);
+		assertEquals(60,cal.sub(90, 30),0);
 	}
 	
 	@Test
 	public void testMul() {
-		Calcurator c = new Calcurator();
-		assertEquals(60,c.mul(2, 30),0);
+		assertEquals(60,cal.mul(2, 30),0);
 	}
 	
 	@Test
 	public void testDiv() throws Exception {
-		Calcurator c = new Calcurator();
-		assertEquals(60,c.div(60, 1),0);
+		assertEquals(60,cal.div(60, 1),0);
+	}
+	
+	@Test
+	public void test4Cal() {
+		assertEquals(20,cal.sub(cal.add(10, 40),cal.sub(40,10)),0);
 	}
 }
